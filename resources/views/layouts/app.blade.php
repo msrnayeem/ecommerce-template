1efe0e7c6a74684a4c3184824a12562e1311b4b2
msrnayeem <msrnayeem@gmail.com>
2025-08-03 17:18:50 +0600

    meta, gtm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,23 @@
 @include('layouts.includes.head')
 
 <body class="home-page">
+    <!-- Google Tag Manager (noscript) -->
+    @if (isset($marketingIntegration) && $marketingIntegration->google_tag_manager_id)
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id={{ $marketingIntegration->google_tag_manager_id }}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+    @endif
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- Meta Pixel (noscript) -->
+    @if (isset($marketingIntegration) && $marketingIntegration->meta_pixel_id)
+        <noscript>
+            <img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id={{ $marketingIntegration->meta_pixel_id }}&ev=PageView&noscript=1" />
+        </noscript>
+    @endif
+    <!-- End Meta Pixel (noscript) -->
     @include('layouts.includes.header')
 
     <div class="main-body-wrapper">
