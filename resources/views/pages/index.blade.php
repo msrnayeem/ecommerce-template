727e7050ae8568663baabc518e585bc71e97c5a7
msrnayeem <msrnayeem@gmail.com>
2025-08-05 11:44:48 +0600

    banner updated

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <!-- Banner Section -->
     @php
-        $imageUrl = env('IMAGE_LINK', 'https://metasoftbd.net/storage/');
+        $imageUrl = env('IMAGE_LINK', 'https://metasoftbd.net/');
 
     @endphp
 
